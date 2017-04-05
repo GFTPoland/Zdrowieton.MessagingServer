cefdfd04c15a6896bc899121973cd2beefe11015
nieprzecietny <kowalski@nieprzecietny.pl>
2017-04-05 14:50:45 +0200

    Allow All origins

--- src/main/java/com/gft/zdrowieton/TopicController.java
+++ src/main/java/com/gft/zdrowieton/TopicController.java
@@ -1,9 +1,16 @@
 package com.gft.zdrowieton;
 
+import javafx.beans.value.ObservableListValue;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.SortedList;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class TopicController {
@@ -11,6 +18,8 @@ public class TopicController {
     @MessageMapping("/send/{topicName}")
     @SendTo("/topic/{topicName}")
     public String passThrough(@DestinationVariable String topicName, String message) throws Exception {
+
+
         return message;
     }
 }
